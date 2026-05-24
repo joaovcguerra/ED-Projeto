46e9774b8f0092c8d56e7887d88da4d4419619aa
João Vitor <99694870+joaovcguerra@users.noreply.github.com>
2026-05-24 12:30:51 -0300

    insere e busca hash

--- main.c
+++ main.c
@@ -33,3 +33,43 @@ Hash criaHash() {
     }
     return h;
 }
+
+int insereHash(Hash h, char *user, char *pass) {
+    if (h == NULL) return 1;
+
+    int pos = valorString(user) % TABLE_SIZE;
+
+    Elem *no = malloc(sizeof(Elem));
+    if (no != NULL) {
+        strcpy(no->username, user);
+        no->hash_senha = valorString(pass);
+
+        no->prox = h->tabela[pos];
+        h->tabela[pos] = no;
+
+        printf("\nUsuario '%s' cadastrado com sucesso!\n", user);
+        return 0;
+    }
+    return 1;
+}
+
+int buscaHash(Hash h, char *user, char *pass) {
+    if (h == NULL) return 1;
+
+    int pos = valorString(user) % TABLE_SIZE;
+    Elem *aux = h->tabela[pos];
+    long senhaDigitada = valorString(pass);
+
+    while (aux != NULL) {
+        if (strcmp(aux->username, user) == 0) {
+            if (aux->hash_senha == senhaDigitada) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+        aux = aux->prox;
+    }
+    return 1;
+}
+
